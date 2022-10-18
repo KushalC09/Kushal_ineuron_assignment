803ea37729d4c3133c7344ac6a906ea41cdaf197
Kushal Chaple <111130286+KushalC09@users.noreply.github.com>
2022-10-18 19:55:20 +0530

    Add files via upload

---
+++ Python Basic assignment 9.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. 1. To what does a relative path refer?
+
+#A relative path in Python is a path that describes the location of a directory relative to the entry point where you run the Python script.
+
+
+# In[2]:
+
+
+#2. What does an absolute path start with your operating system?
+
+#Absolute path names always begin with the slash (/) symbol. Traces the path from the current directory through its parent or its subdirectories and files.
+
+
+# In[3]:
+
+
+#3. What do the functions os.getcwd() and os.chdir() do?
+
+#os. getcwd() method tells us the location of current working directory (CWD).
+#chdir() method in Python used to change the current working directory to specified path.
+
+
+# In[4]:
+
+
+#4. What are the . and .. folders?
+
+#The . is the current directory, while .. signifies the parent directory.
+
+
+# In[1]:
+
+
+#5.In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+
+#Here, C:\bacon\eggs is the dir name, while spam. txt is the base name.
+
+
+# In[2]:
+
+
+#6. What are the three “mode” arguments that can be passed to the open() function?
+
+#The three "mode" argument to open() functions are r, rb, r+ .
+
+
+# In[3]:
+
+
+#7. What happens if an existing file is opened in write mode?
+
+#If a file with the same name already exists, its contents are discarded and the file is treated as a new empty file.
+
+
+# In[4]:
+
+
+#8. How do you tell the difference between read() and readlines()?
+
+#The read() method returns the specified number of bytes from the file, whereas readlines() method will return all the lines in a file in the format of a list where each element is a line in the file.
+
+
+# In[ ]:
+
+
+#9. What data structure does a shelf value resemble?
+
+#A shelf value resembles a dictionary value; it has keys and values, along with keys() and values() methods that work similarly to the dictionary methods of the same names.
+
